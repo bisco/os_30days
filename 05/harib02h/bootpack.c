54d5944b8d9f5da1ae672a064949d57ecabc45fa
bisco <mail@nbis.co>
2019-10-17 23:08:59 +0900

    add simple and tiny sprintf

--- 05/harib02h/bootpack.c
+++ 05/harib02h/bootpack.c
@@ -34,15 +34,21 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 
+void mysprintf(char *str, char *fmt, ...);
+
 
 void HariMain(void) {
     struct BOOTINFO *binfo = (struct BOOTINFO *)0x0ff0;
+    char s[40];
 
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
     putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
     putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
     putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
+
+    mysprintf(s, "scrnx = %d", binfo->scrnx);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
     for(;;) {
         io_hlt();
