ffdebf8b140e8993c0bc38250698cde59bd32380
bisco <mail@nbis.co>
2019-10-23 10:05:21 +0900

    add message

--- 06/harib03e/bootpack.c
+++ 06/harib03e/bootpack.c
@@ -11,10 +11,15 @@ void HariMain(void) {
 
     init_palette();
     init_screen8(binfo->vram, binfo->scrnx, binfo->scrny);
+
+    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Hello nbisco OS");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Hello nbisco OS");
+
     mx = (binfo->scrnx - 16) / 2;
     my = (binfo->scrny - 28 - 16) / 2;
     init_mouse_cursor8(mcursor, COL8_008484);
     putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
     mysprintf(s, "(%d, %d)", mx, my);
     putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
 
