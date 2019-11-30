a80b89b71874be3430159a2700f589f1d93fa545
bisco <mail@nbis.co>
2019-11-30 10:25:36 +0900

    finish day17 part a: add an idle task

---
+++ 17/harib14a/mouse.c
@@ -0,0 +1,75 @@
+#include "bootpack.h"
+
+#define KEYCMD_SENDTO_MOUSE     (0xd4)
+#define MOUSECMD_ENABLE         (0xf4)
+
+struct FIFO32 *mousefifo;
+int mousedata0;
+
+void inthandler2c(int *esp) {
+    // PS/2マウスからの割り込み
+    unsigned char data;
+    io_out8(PIC1_OCW2, 0x64);   // IRQ12受付完了をPIC1に通知
+    io_out8(PIC0_OCW2, 0x62);   // IRQ02受付完了をPIC1に通知
+    data = io_in8(PORT_KEYDAT);
+    fifo32_put(mousefifo, data + mousedata0);
+    return;
+}
+
+void enable_mouse(struct FIFO32* fifo, int data0, struct MOUSE_DEC *mdec) {
+    // 書き込み先のFIFOバッファアドレスを記憶
+    mousefifo = fifo;
+    mousedata0 = data0;
+    // マウス有効化
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+    // うまくいくとACK(0xfa)が返ってくる
+    mdec->phase = 0; // マウスの0xfaを待っている段階
+    return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat) {
+    if(mdec->phase == 0) {
+        // ACK(0xfa)を読み捨てる
+        if(dat == 0xfa) {
+            mdec->phase = 1;
+        }
+        return 0;
+    }
+    if(mdec->phase == 1) {
+        // 1バイト目を待っている
+        if((dat & 0xc8) == 0x08) {
+            // 正しい1バイト目だった
+            mdec->buf[0] = dat;
+            mdec->phase = 2;
+        }
+        return 0;
+    }
+    if(mdec->phase == 2) {
+        // 2バイト目を待っている
+        mdec->buf[1] = dat;
+        mdec->phase = 3;
+        return 0;
+    }
+    if(mdec->phase == 3) {
+        // 3バイト目を待っている
+        mdec->buf[2] = dat;
+        mdec->phase = 1;
+        mdec->btn = mdec->buf[0] & 0x07;
+        mdec->x = mdec->buf[1];
+        mdec->y = mdec->buf[2];
+        if((mdec->buf[0] & 0x10) != 0) {
+            mdec->x |= 0xffffff00;
+        }
+        if((mdec->buf[0] & 0x20) != 0) {
+            mdec->y |= 0xffffff00;
+        }
+        mdec->y = -mdec->y; // マウスのy方向の符号は画面と反対
+        return 1;
+    }
+    return -1; // ここにはこない
+
+
+}
